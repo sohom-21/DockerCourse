f1d16859c36983cab34bbb0c4e8d8f9e1409b2c2
Sohom Ghosh <sohom.official21@gmail.com>
2025-03-29 23:05:13 +0530

    Add initial backend and frontend structure with environment configurations

--- mern-docker/frontend/vite.config.js
+++ mern-docker/frontend/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+  server:{
     watch:{
       usePolling: true,
-      interval: 100
+      interval: 100,
     }
-  },
+  }
 })
